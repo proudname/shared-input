fbd53cf09aa4a3bb673d90418fa4afb85cb3985f
aleksejgriskovec <proudname813@gmail.com>
2022-12-07 22:14:26 +0400

    project init

---
+++ server/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import http from 'http';
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(process.env.PORT, () => {
+    console.log(`listening on *:${process.env.PORT}`);
+});
